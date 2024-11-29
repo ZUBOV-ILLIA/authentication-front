760f9ac5a7d36311dbbaf5304cb1667abf3e8986
ZUBOV ILLIA <zubovillia@gmail.com>
2024-11-29 12:15:01 +0300

    small refactoring

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,27 +2,25 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <nav className="navbar" role="navigation" aria-label="main navigation">
-      <div id="navbarBasicExample" className="navbar-menu">
-        <div className="navbar-start">
-          <Link to="/" className="navbar-item">
-            Home
-          </Link>
-          <Link to="/users" className="navbar-item">
-            Users
-          </Link>
-        </div>
+    <nav className="nav" role="navigation" aria-label="main navigation">
+      <div className="nav__side">
+        <Link to="/" className="navbar-item">
+          Home
+        </Link>
+        <Link to="/users" className="navbar-item">
+          Users
+        </Link>
+      </div>
 
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons">
-              <Link to="/sign-up" className="button is-primary">
-                Sign up
-              </Link>
-              <Link to="/log-in" className="button is-light">
-                Log in
-              </Link>
-            </div>
+      <div className="nav__side">
+        <div className="navbar-item">
+          <div className="buttons">
+            <Link to="/sign-up" className="button is-primary">
+              Sign up
+            </Link>
+            <Link to="/log-in" className="button is-light">
+              Log in
+            </Link>
           </div>
         </div>
       </div>
